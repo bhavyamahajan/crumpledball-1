48dfeec23368fc3cf1af085dd34c1439878e0481
bhavyamahajan <71073820+bhavyamahajan@users.noreply.github.com>
2020-11-15 12:23:56 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,10 @@
+class Ground{
+constructor(){x,y,width,length}
+width=body.width;
+length=body.length;
+
+ground=createsprite(800,350,50,100)
+ground.shapecolor(brown)
+
+
+}
---
+++ paper.js
@@ -0,0 +1,20 @@
+class Paper{
+constructor(){radius,options}
+  options={
+isStatic:false,
+restitution:0.3,
+friction:0.5,
+density:1.2,
+}
+
+    
+
+
+
+
+
+
+
+
+
+}
